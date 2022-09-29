7b23b5c915a1c5f6ec632103abd685c5fea40bde
cesarnr21 <cesarnr21@gmail.com>
2022-09-28 21:17:10 -0700

    send emails functionality

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# google_services
+# Google Services
+Just some modules that allow to use some Google Services. To Google Services and APIs, they needs to be setup
+- instructions here: <https://www.thepythoncode.com/article/use-gmail-api-in-python>
+
+Then download the API credentials to a `json` file and add it to the working directory.
+
+## Gmail Module
+The `gmail_services.py` includes the class `gmail_action` which allows to send emails and attachments. Functions in the class:
+- `build_message(subject = 'str', content = 'str', attachments = 'str or tuple')`. This is used to put together the message being sent.
+- Another way to add attachements to the email message is to use the `add_attachments()` function. Argument can either be a `string` for a single attachment or a `tuple` for multiple. It is important to add the full path of the attachment. The `os.path.anspath()` can be used to help with this.
+- `send_email()` can be used after building a message to send the email. The arguments are the source email and a destination email address.
+```py
+from gmail_services import gmail_action
+
+email = gmail_action(creds_file = 'credentials.json')
+
+email.build_message(subject = ' ', content = ' ', attachments = ('file1', 'file2'))
+email.add_attachments(attachments = ('files', 'files'))
+email.send_email('source_email', 'target_email')
+```
+
+## Google Drive and Google Sheets
+
+# TO DO
+- add instructions to creating google credentials
+- change how text files are sent, maybe remove all the options for storing different files
+- try sending videos, and other files
+- Is `create_draft()` neccessary
+- Add functionality to receive emails and proccess them
+
+
--- lib/gmail_services.py
+++ lib/gmail_services.py
@@ -1,4 +1,6 @@
 
+from base64 import urlsafe_b64decode
+from email import message
 from google_services import google_service
 
 class gmail_action(google_service):
@@ -7,22 +9,77 @@ class gmail_action(google_service):
         app_scope = ['https://mail.google.com/']
         google_service.__init__(self, creds_file, app_scope)
         from googleapiclient.discovery import build
-        build('gmail', 'v1', credentials = self.creds)
+        self.service = build('gmail', 'v1', credentials = self.creds)
 
 
-    def build_message(self, content, subject = '', attachments = ''):
+    def build_message(self, content, subject = '', attachments = {}):
         # might remove these lines below
         if subject == '':
             subject = 'no_subject'
 
-        message = {'subject' : subject, 'content' : content, 'attachments' : attachments}
-        return message
+        if len(attachments) != 0:
+            attach_buf = {}
+
+            if len(attachments) != 1:
+                attach_buf[0] = attachments
+            elif len(attachments) != 0:
+                for i in range(len(attachments)):
+                    attach_buf[i] = attachments[i]
+
+            self.message = {'subject' : subject, 'content' : content, 'attachments' : attach_buf}
+
+        else:
+            self.message = {'subject' : subject, 'content' : content, 'attachments' : attachments}
+
+        return self.message
+
+    
+    def build_attach(self, mail, file):
+        from email.mime.text import MIMEText
+        from email.mime.image import MIMEImage
+        from email.mime.audio import MIMEAudio
+        from email.mime.base import MIMEBase
+        from mimetypes import guess_type as guess_mime_type
+        import os
+
+        os.path.abspath('file1.txt')
+
+        content_type, encoding = guess_mime_type(file)
+        if content_type is None or encoding is not None:
+            content_type = 'application/octet-stream'
+        main_type, sub_type = content_type.split('/', 1)
+        try:
+            if main_type == 'text':
+                fp = open(file, 'rb')
+                msg = MIMEText(fp.read().decode(), _subtype=sub_type)
+                fp.close()
+            elif main_type == 'image':
+                fp = open(file, 'rb')
+                msg = MIMEImage(fp.read(), _subtype=sub_type)
+                fp.close()
+            elif main_type == 'audio':
+                fp = open(file, 'rb')
+                msg = MIMEAudio(fp.read(), _subtype=sub_type)
+                fp.close()
+            else:
+                fp = open(file, 'rb')
+                msg = MIMEBase(main_type, sub_type)
+                msg.set_payload(fp.read())
+                fp.close()
+
+            file = os.path.basename(file)
+            msg.add_header('Content-Disposition', 'attachment', file = file)
+            mail.attach(msg)
+
+        except FileNotFoundError:
+            print("ERROR: Attachments not found. Make sure you specify full path of file you want to attach")
 
 
-    def create_draft(self, message):
+
+    def create_draft(self):
         """docs here: https://developers.google.com/gmail/api/guides/drafts"""
         print("create draft test:\n------------------------")
-        print(message)
+        print(self.message)
         # import base64
         # from email.message import EmailMessage
 
@@ -34,61 +91,49 @@ class gmail_action(google_service):
         #     draft = None
 
         # return draft
-        return message
+        return self.message
 
 
-    def send_email(self, target_email, message):
+    def send_email(self, source_mail, target_mail):
         """docs here: https://developers.google.com/gmail/api/guides/sending"""
         print("send email test:\n------------------------------")
-        print("Target Email:", target_email)
-        print("Message", message)
+        print("Target Email:", target_mail)
+        print("Message", self.message)
         import base64
-        from email.message import EmailMessage
-
         from email.mime.text import MIMEText
+        from email.mime.multipart import MIMEMultipart
+
+        if not self.message['attachments']:
+            mail = MIMEText(self.message['content'])
+            mail['To'] = target_mail
+            mail['From'] = source_mail
+            mail['Subject'] = self.message['subject']
+
+        else:
+            mail = MIMEMultipart()
+            mail['To'] = target_mail
+            mail['From'] = source_mail
+            mail['Subject'] = self.message['subject']
+            mail.attach(MIMEText(self.message['content']))
+
+            for i in range(len(self.message['attachments'])):
+                self.build_attach(mail, self.message['attachments'][i])
+
+        self.service.users().messages().send(userId = 'me', body = {'raw': base64.urlsafe_b64encode(mail.as_bytes()).decode()}).execute()
 
 
-        email = EmailMessage()
-
-        email.set_content(message['content'])
-
-        email['To'] = target_email
-        email['From'] = 'source_email'
-        email['Subject'] = message['subject']
-
-        place_hold = False
-        if place_hold:
-            from email.mime.multipart import MIMEMultipart
-            from email.mime.image import MIMEImage
-            from email.mime.audio import MIMEAudio
-            from email.mime.base import MIMEBase
-            from mimetypes import guess_type as guess_mime_type
-
-
-        # pass
-
-
-    def add_attachemnts(self, message, attachments):
+    def add_attachments(self, attachments = ()):
+        index = len(self.message['attachments'])
         for i in range(len(attachments)):
-            message['attachments'][i] = attachments[i]
+            self.message['attachments'][index + i] = attachments[i]
 
-            """working theory for already having attachments"""
-            """
-            index = len(message['attachments']) # get the lenght of the current attachments
-            message['attachments'][index] = attachments[i]
-            """
-
-        return message
-
-
-    def find_email(self):
-        pass
+        return self.message
 
 
 
 
 
-class receive_mail():
+class receive_mail(gmail_action):
     """docs here: https://developers.google.com/gmail/api/guides/push"""
-    def __init__(self):
-        pass
+    def __init__(self, creds_file):
+        gmail_action.__init__(self, creds_file)
